5a57870fe32c9e62e49060c0e45478eb98169e90
yerimyerim <vigvang_bip@naver.com>
2021-09-26 12:17:04 +0900

    StartSceneManager script add

---
+++ Assets/Script/StartScene/StartPanelManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPanelManager : MonoBehaviour
+{
+    [SerializeField] private Button StartButton;
+    [SerializeField] private Button ExitButton;
+    [SerializeField] private Button StoreButton;
+    [SerializeField] private Button SettingButton;
+    
+    
+
+}
