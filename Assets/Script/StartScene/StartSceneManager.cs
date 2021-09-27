7d9b9b154a84c0cb47fd4b107418be6d45e19973
yerimyerim <vigvang_bip@naver.com>
2021-09-27 18:11:21 +0900

    animating add

--- Assets/Script/StartScene/StartSceneManager.cs
+++ Assets/Script/StartScene/StartSceneManager.cs
@@ -29,17 +29,19 @@ public class StartSceneManager : MonoBehaviour
     [SerializeField] private Text TurretShootBeforeSpeedText;    
     [SerializeField] private Text TurretShootAfterSpeedText;    
     [SerializeField] private Text TurretLevelText;
+
+
+    [Header("Animator")] 
     
-    
-    
-    
+    [SerializeField] private Animator CraftZoneAnimator;
+    [SerializeField] private Animator StartSceneAnimator;   
     void Awake()
     {
         StartPannel = GameObject.Find("StartPanel");
         CraftZonePannel = GameObject.Find("CraftZonePanel");
         ShipObject = GameObject.Find("ShipModel");
-        
         StartButton = GameObject.Find("StartButton").GetComponent<Button>();
+
         ExitButton = GameObject.Find("ExitButton").GetComponent<Button>();
         StoreButton = GameObject.Find("StoreButton").GetComponent<Button>();
         SettingButton = GameObject.Find("Settingbutton").GetComponent<Button>();
@@ -47,14 +49,38 @@ public class StartSceneManager : MonoBehaviour
         UpgradeButton = GameObject.Find("UpgradeButton").GetComponent<Button>();
         GameStartButton = GameObject.Find("GameStartButton").GetComponent<Button>();
         
+        
+        
         HaveToPayGoldText = GameObject.Find("HaveToPayGoldText").GetComponent<Text>();
         TurretNameText = GameObject.Find("TurretNameText").GetComponent<Text>();
         TurretInfoText = GameObject.Find("TurretInfoText").GetComponent<Text>();
         TurretDamegeBeforeText = GameObject.Find("TurretDamegeBeforeText").GetComponent<Text>();
         TurretDamegeAfterText = GameObject.Find("TurretDamegeAfterText").GetComponent<Text>();
         TurretShootBeforeSpeedText = GameObject.Find("TurretShootBeforeSpeedText").GetComponent<Text>();
-        TurretShootAfterSpeedText = GameObject.Find("TurretShootAfterSpeedText").GetComponent<Text>();
+        TurretShootAfterSpeedText = GameObject.Find("TurretShootAfterSpeedText").GetComponent<Text>();    
         TurretLevelText = GameObject.Find("NowSelectTurretLevelText").GetComponent<Text>();
+
+
+        CraftZoneAnimator = CraftZonePannel.GetComponent<Animator>();
+        StartSceneAnimator = StartPannel.GetComponent<Animator>();
         
+        
+        if (StartButton != null)
+        {
+            StartButton.onClick.AddListener(delegate { StartButtonOnClick(); });
+        }
+
+        
+        CraftZonePannel.SetActive(false);
+    }
+
+    private void StartButtonOnClick()
+    {              
+        StartSceneAnimator.SetBool("isOut", true);
+        CraftZonePannel.SetActive(true);  
+        CraftZoneAnimator.SetBool("isEnter", true);
+        ShipObject.transform.Rotate(0,90,0);
+
+
     }
 }
